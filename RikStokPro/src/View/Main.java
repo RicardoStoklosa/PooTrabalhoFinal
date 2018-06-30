e9ffbc7ccc5ce6942cec602d6246768c515c6b1c
Ricardo Stoklosa <stoklosa.ricardo@gmail.com>
2018-06-30 14:01:33 -0300

    DAOArquivo

--- RikStokPro/src/View/Main.java
+++ RikStokPro/src/View/Main.java
@@ -7,22 +7,30 @@ package View;
 import EDA.*;
 import DAO.*;
 import Negocio.*;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  *
  * @author stokl
  */
 public class Main {
-    
-    public static void main(String args[]){
-        NegocioFacade.init();
-        System.out.println(Toolbox.encrypt("oi"));
+    public static NegocioFacade negocio = new NegocioFacade();
+    public static void main(String args[]) throws IOException{
+        negocio.init();
+//        negocio.addProduto(new Frios(-30.00, "presunto", 75, 45, 1.20));
+//        negocio.addProduto(new Liquido(1.00, "leite", 64, 12, 2.20));
+//        negocio.addProduto(new Secos(10.00, "arroz", 15, 7, 4.00));
+//        
+                
         ArrayList<Produto> produt= NegocioFacade.getProdutos();
         for( Produto prod : produt){
             System.out.println(prod);
             
         }
+       negocio.exit();
+        
     }
     
 }
